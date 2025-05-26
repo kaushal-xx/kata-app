41a7e9027a0c696398683fec6bf9c02d72952bad
kaushal <kaushalkishorsharma@kaushals-MacBook-Pro-2.local>
2025-05-26 16:00:02 +0530

    Updated Rspec test case and more scenarios

--- spec/lib/core_extensions/string_spec.rb
+++ spec/lib/core_extensions/string_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'String#add' do
   describe '#add' do
     it 'returns 0 for an empty string' do
       expect("".add).to eq(0)
+      expect("   ".add).to eq(0)
     end
 
     it 'returns the number itself for a single number' do
@@ -47,6 +48,32 @@ RSpec.describe 'String#add' do
 
     it 'raises an error for negative numbers with special character' do
       expect{ "$-1*,$2(,3#".add }.to raise_error("negative numbers not allowed: -1")
+    end
+  end
+
+  describe '#extract_numbers' do
+    it 'extracts integers from a string' do
+      expect("abc123xyz456".extract_numbers).to eq([123, 456])
+    end
+
+    it 'extracts floats from a string' do
+      expect("price: 12.5, tax: 2.5".extract_numbers).to eq([12.5, 2.5])
+    end
+
+    it 'handles negative numbers' do
+      expect("profit: -40, loss: -3.5".extract_numbers).to eq([-40, -3.5])
+    end
+
+    it 'returns empty array if no numbers exist' do
+      expect("nothing here!".extract_numbers).to eq([])
+    end
+
+    it 'mixes integers and floats' do
+      expect("a1 b2.5 c3".extract_numbers).to eq([1, 2.5, 3])
+    end
+
+    it 'ignores non-numeric characters' do
+      expect("$20.5 + @3 and -1.1!".extract_numbers).to eq([20.5, 3, -1.1])
     end
   end
 end
